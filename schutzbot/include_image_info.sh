633f0919f3d90b3bd7a9d84bc9d9e9fe0039c098
Thomas Lavocat <tlavocat@redhat.com>
2022-07-04 18:01:53 +0200

    db: workflow to update the database
    First a github action pulls the new manifests from composer. Creates a
    commit out of the modification and pushes these to the gitlab repository
    on a specific branch. Then it triggers a pipeline using the trigger
    token.
    
    Then the CI runs in "trigger" mode. In this mode it will compile all the
    new manifests and produce the corresponding image-info in the artifacts.
    
    Once this is done, in the final phase the CI will download all the
    produced image-info and ammend the update db commit with them. Then it
    will push that on github and open a PR.

---
+++ schutzbot/include_image_info.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+set -euxo pipefail
+sudo dnf install gh -y
+sudo pip install python-gitlab
+
+# Login to GH
+echo "${SCHUTZBOT_GH_TOKEN}" > /tmp/secret.txt
+gh auth login --with-token < /tmp/secret.txt
+
+# import the image info from the current build
+./tools/ci_import --pipeline-id "$CI_PIPELINE_ID" --token  ${GITLAB_TOKEN} --verbose
+
+#remove ci-details-before-run
+mv ci-details-before-run /tmp
+rm -rf generated-image-infos
+
+# Amend the commit containing the db update
+git checkout $CI_COMMIT_BRANCH
+
+git config --local user.name "SchutzBot"
+git config --local user.email "schutzbot@redhat.com"
+
+git remote add origin2 https://schutzbot:"$SCHUTZBOT_GH_TOKEN"@github.com/schutzbot/manifest-db.git
+
+git add -A && \
+    git commit --amend -m "db: update
+
+Automatic update:
+- manifests from latest composer
+- image-info from pipeline $CI_PIPELINE_ID" && \
+    git push --set-upstream origin2 $CI_COMMIT_BRANCH && \
+    gh pr create \
+        --title "db update" \
+        --body "automated db update" \
+        --repo "osbuild/manifest-db" \
+        -r lavocatt
+
+#restore ci-details-before-run
+mv /tmp/ci-details-before-run .
