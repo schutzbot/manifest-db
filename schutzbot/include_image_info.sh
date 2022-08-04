04a9ae4f09816887929dfb666cf456c197233f2b
Thomas Lavocat <tlavocat@redhat.com>
2022-08-04 11:10:32 +0200

    test

--- schutzbot/include_image_info.sh
+++ schutzbot/include_image_info.sh
@@ -6,12 +6,15 @@ sudo pip install python-gitlab
 echo "${SCHUTZBOT_GH_TOKEN}" > /tmp/secret.txt
 gh auth login --with-token < /tmp/secret.txt
 
-./tools/ci_import --pipeline-id "$CI_PIPELINE_ID" --token  ${GITLAB_TOKEN} --verbose
+#./tools/ci_import --pipeline-id "$CI_PIPELINE_ID" --token  ${GITLAB_TOKEN} --verbose
 
 git checkout $CI_COMMIT_BRANCH
 
 git config --local user.name "SchutzBot"
 git config --local user.email "schutzbot@redhat.com"
+
+git remote add origin2 https://@github.com/schutzbot/manifest-db.git
+touch toto
 
 # only change the last commit and push to github if things were changed
 git diff-index --quiet HEAD -- || git add -A && \
@@ -20,7 +23,7 @@ git diff-index --quiet HEAD -- || git add -A && \
 Automatic update:
 - manifests from latest composer
 - image-info from pipeline $CI_PIPELINE_ID" && \
-    git push https://"$SCHUTZBOT_LOGIN"@github.com/schutzbot/manifest-db.git $CI_COMMIT_BRANCH && \
+    git push https://"$SCHUTZBOT_PUSH_TOKEN"@github.com/schutzbot/manifest-db.git $CI_COMMIT_BRANCH && \
     gh pr create \
         --title "db update" \
         --body "automated db update" \
