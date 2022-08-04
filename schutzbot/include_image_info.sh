d35b62de3151caecaaa758d6d446dbf48e623ea4
Thomas Lavocat <tlavocat@redhat.com>
2022-08-04 11:10:32 +0200

    test

--- schutzbot/include_image_info.sh
+++ schutzbot/include_image_info.sh
@@ -3,27 +3,36 @@ set -euxo pipefail
 sudo dnf install gh -y
 sudo pip install python-gitlab
 
+# Login to GH
 echo "${SCHUTZBOT_GH_TOKEN}" > /tmp/secret.txt
 gh auth login --with-token < /tmp/secret.txt
 
+# import the image info from the current build
 ./tools/ci_import --pipeline-id "$CI_PIPELINE_ID" --token  ${GITLAB_TOKEN} --verbose
 
+#remove ci-details-before-run
+mv ci-details-before-run /tmp
+
+# Amend the commit containing the db update
 git checkout $CI_COMMIT_BRANCH
 
 git config --local user.name "SchutzBot"
 git config --local user.email "schutzbot@redhat.com"
 
-# only change the last commit and push to github if things were changed
-git diff-index --quiet HEAD -- || git add -A && \
+git remote add origin2 https://schutzbot:"$SCHUTZBOT_GH_TOKEN"@github.com/schutzbot/manifest-db.git
+
+git add -A && \
     git commit --amend -m "db: update
 
 Automatic update:
 - manifests from latest composer
 - image-info from pipeline $CI_PIPELINE_ID" && \
-    git push https://"$SCHUTZBOT_LOGIN"@github.com/schutzbot/manifest-db.git $CI_COMMIT_BRANCH && \
+    git push --set-upstream origin2 $CI_COMMIT_BRANCH && \
     gh pr create \
         --title "db update" \
         --body "automated db update" \
         --repo "osbuild/manifest-db" \
-        --base "main" \
         -r lavocatt
+
+#restore ci-details-before-run
+mv /tmp/ci-details-before-run .
