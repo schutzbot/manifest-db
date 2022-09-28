e6d9ce7d31d64b895889cb21d9f40b916b9dbabc
Thomas Lavocat <tlavocat@redhat.com>
2022-09-28 16:56:12 +0200

    commons: add the default target element

--- image_info/image_info/common/env.py
+++ image_info/image_info/common/env.py
@@ -2,6 +2,7 @@
 environment
 """
 from attr import define
+from image_info.utils.process import subprocess_check_output
 from image_info.utils.utils import parse_environment_vars
 from image_info.report.common import Common
 
@@ -22,3 +23,35 @@ class OsRelease(Common):
     @classmethod
     def from_json(cls, json_o):
         return cls(json_o["os-release"])
+
+
+@define(slots=False)
+class DefaultTarget(Common):
+    """
+    Default systemd target
+    """
+    flatten = True
+    default_target: str
+
+    @classmethod
+    def explore(cls, tree, _is_ostree=False):
+        """
+        Read the default systemd target.
+
+        Returns: string representing the default systemd target.
+
+        An example return value:
+        "multi-user.target"
+        """
+        default_target = subprocess_check_output(
+            ["systemctl", f"--root={tree}", "get-default"]).rstrip()
+        if default_target:
+            return cls(default_target)
+        return None
+
+    @classmethod
+    def from_json(cls, json_o):
+        default_target = json_o.get("default-target")
+        if default_target:
+            return cls(default_target)
+        return None
