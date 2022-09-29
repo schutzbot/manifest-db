ab5844e5cbc68461a58aa381c2517c0889f13629
Thomas Lavocat <tlavocat@redhat.com>
2022-09-29 13:05:59 +0200

    commons: add the hosts element

--- image_info/image_info/common/network.py
+++ image_info/image_info/common/network.py
@@ -3,6 +3,7 @@ Network config
 """
 
 import xml.etree.ElementTree
+import contextlib
 from attr import define
 from image_info.report.common import Common
 from image_info.utils.utils import parse_environment_vars
@@ -97,4 +98,46 @@ class FirewallEnabled(Common):
         fwe = json_o.get("firewall-enabled")
         if fwe:
             return cls(fwe)
+        return None
+
+
+@define(slots=False)
+class Hosts(Common):
+    """
+    Hosts
+    """
+    flatten = True
+    hosts: list[str]
+
+    @classmethod
+    def explore(cls, tree, _is_ostree=False):
+        """
+        Read non-empty lines of /etc/hosts.
+
+        Returns: list of strings for all uncommented lines in the configuration file.
+        The returned list may be empty.
+
+        An example return value:
+        [
+            "127.0.0.1   localhost localhost.localdomain localhost4 localhost4.localdomain4",
+            "::1         localhost localhost.localdomain localhost6 localhost6.localdomain6"
+        ]
+        """
+        result = []
+
+        with contextlib.suppress(FileNotFoundError):
+            with open(f"{tree}/etc/hosts") as f:
+                for line in f:
+                    line = line.strip()
+                    if line:
+                        result.append(line)
+        if result:
+            return cls(result)
+        return None
+
+    @classmethod
+    def from_json(cls, json_o):
+        hosts = json_o.get("hosts")
+        if hosts:
+            return cls(hosts)
         return None
