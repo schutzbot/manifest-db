1eff4d304f1091418998adb05ce222fba7eed943
Thomas Lavocat <tlavocat@redhat.com>
2022-09-29 10:15:50 +0200

    commons: add the chrony element

--- image_info/image_info/common/conf.py
+++ image_info/image_info/common/conf.py
@@ -48,3 +48,56 @@ class Authselect(Common):
     @classmethod
     def from_json(cls, json_o):
         return cls(json_o["enabled-features"], json_o["profile-id"])
+
+
+@define(slots=False)
+class Chrony(Common):
+    """
+    Chrony
+    """
+    flatten = True
+    chrony: dict
+
+    @classmethod
+    def explore(cls, tree, _is_ostree=False):
+        """
+        Read specific directives from Chrony configuration. Currently parsed
+        directives are:
+        - 'server'
+        - 'pool'
+        - 'peer'
+        - 'leapsectz'
+
+        Returns: dictionary with the keys representing parsed directives from Chrony
+        configuration. Value of each key is a list of strings containing arguments
+        provided with each occurance of the directive in the configuration.
+        """
+        chrony = {}
+        parsed_directives = ["server", "pool", "peer", "leapsectz"]
+        with contextlib.suppress(FileNotFoundError):
+            #pylint: disable = unspecified-encoding
+            with open(f"{tree}/etc/chrony.conf") as f:
+                for line in f:
+                    line = line.strip()
+                    if not line:
+                        continue
+                    # skip comments
+                    if line[0] in ["!", ";", "#", "%"]:
+                        continue
+                    split_line = line.split()
+                    if split_line[0] in parsed_directives:
+                        try:
+                            directive_list = chrony[split_line[0]]
+                        except KeyError:
+                            directive_list = chrony[split_line[0]] = []
+                        directive_list.append(" ".join(split_line[1:]))
+        if chrony:
+            return cls(chrony)
+        return None
+
+    @classmethod
+    def from_json(cls, json_o):
+        chrony = json_o.get("chrony")
+        if chrony:
+            return cls(chrony)
+        return None
