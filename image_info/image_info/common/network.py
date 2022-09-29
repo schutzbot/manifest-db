3c9223fda6baf0066ae9ed92dce17c2ae88bcf48
Thomas Lavocat <tlavocat@redhat.com>
2022-09-29 14:39:35 +0200

    commons: add the resolvconf element

--- image_info/image_info/common/network.py
+++ image_info/image_info/common/network.py
@@ -96,7 +96,7 @@ class FirewallEnabled(Common):
     @classmethod
     def from_json(cls, json_o):
         fwe = json_o.get("firewall-enabled")
-        if fwe:
+        if fwe or fwe == []:  # legacy requires also empty tables
             return cls(fwe)
         return None
 
@@ -175,3 +175,47 @@ class MachineId(Common):
         if machine_id:
             return cls(machine_id)
         return cls("")  # in the legacy image-info the value exists even empty
+
+
+@define(slots=False)
+class ResolvConf(Common):
+    """
+    ResolvConf
+    """
+    flatten = True
+    _l_etc_l_resolv__conf: str
+
+    @classmethod
+    def explore(cls, tree, _is_ostree=False):
+        """
+        Read /etc/resolv.conf.
+
+        Returns: a list of uncommented lines from the /etc/resolv.conf.
+
+        An example return value:
+        [
+            "search redhat.com",
+            "nameserver 192.168.1.1",
+            "nameserver 192.168.1.2"
+        ]
+        """
+        result = []
+
+        with contextlib.suppress(FileNotFoundError):
+            with open(f"{tree}/resolv.conf") as f:
+                for line in f:
+                    line = line.strip()
+                    if not line:
+                        continue
+                    if line[0] == "#":
+                        continue
+                    result.append(line)
+
+        if result:
+            return cls(result)
+        return cls([])   # The legacy requires even an empty resolvonf array
+
+    @classmethod
+    def from_json(cls, json_o):
+        resolv = json_o.get("/etc/resolv.conf")
+        return cls(resolv)  # The legacy requires even an empty resolvonf array
