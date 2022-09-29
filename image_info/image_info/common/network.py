5cfac96ada03b8c1425265213f944120a8fe5812
Thomas Lavocat <tlavocat@redhat.com>
2022-09-29 13:25:48 +0200

    commons: add the MachineId element

--- image_info/image_info/common/network.py
+++ image_info/image_info/common/network.py
@@ -141,3 +141,37 @@ class Hosts(Common):
         if hosts:
             return cls(hosts)
         return None
+
+
+@define(slots=False)
+class MachineId(Common):
+    """
+    MachineId
+    """
+    flatten = True
+    machine_id: str
+
+    @classmethod
+    def explore(cls, tree, _is_ostree=False):
+        """
+        Read all uncommented key/values set in /etc/locale.conf.
+
+        Returns: dictionary with key/values read from the configuration file.
+        The returned dictionary may be empty.
+
+        An example return value:
+        {
+            "LANG": "en_US"
+        }
+        """
+        with contextlib.suppress(FileNotFoundError):
+            with open(f"{tree}/etc/machine-id") as f:
+                return cls(f.readline())
+        return cls("")  # in the legacy image-info the value exists even empty
+
+    @classmethod
+    def from_json(cls, json_o):
+        machine_id = json_o.get("machine-id")
+        if machine_id:
+            return cls(machine_id)
+        return cls("")  # in the legacy image-info the value exists even empty
