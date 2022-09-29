daff549c9e0112ccfe0804fdf690d9a662f207c0
Thomas Lavocat <tlavocat@redhat.com>
2022-09-29 14:29:22 +0200

    commons: add the SystemdServiceDropins element

--- image_info/image_info/common/conf.py
+++ image_info/image_info/common/conf.py
@@ -1328,3 +1328,84 @@ class SysctlDConfig(Common):
         if config:
             return cls(config)
         return None
+
+
+@define(slots=False)
+class SystemdServiceDropins(Common):
+    """
+    SystemdServiceDropins
+    """
+    flatten = True
+    systemd_service_dropins: dict
+
+    @staticmethod
+    def read_systemd_service_dropin(dropin_dir_path):
+        """
+        Read systemd .service unit drop-in configurations.
+
+        Returns: dictionary representing the combined drop-in configurations.
+
+        An example return value:
+        {
+            "Service": {
+                "Environment": "NM_CLOUD_SETUP_EC2=yes"
+            }
+        }
+        """
+        # read all unit drop-in configurations
+        config_files = glob.glob(f"{dropin_dir_path}/*.conf")
+        parser = configparser.RawConfigParser()
+        # prevent conversion of the opion name to lowercase
+        parser.optionxform = lambda option: option
+        parser.read(config_files)
+
+        dropin_config = {}
+        for section in parser.sections():
+            section_config = {}
+            section_config.update(parser[section])
+            if section_config:
+                dropin_config[section] = section_config
+
+        return dropin_config
+
+    @classmethod
+    def explore(cls, tree, _is_ostree=False):
+        """
+        Read all systemd .service unit config files from a predefined list of paths
+        and parse them.
+
+        The searched paths are:
+        - "/etc/systemd/system/*.service.d"
+        - "/usr/lib/systemd/system/*.service.d"
+
+        Returns: dictionary as returned by '_read_glob_paths_with_parser()' with
+        configuration representation as returned by 'read_systemd_service_dropin()'.
+
+        An example return value:
+        {
+            "/etc/systemd/system": {
+                "nm-cloud-setup.service.d": {
+                    "Service": {
+                        "Environment": "NM_CLOUD_SETUP_EC2=yes"
+                    }
+                }
+            }
+        }
+        """
+        checked_globs = [
+            "/etc/systemd/system/*.service.d",
+            "/usr/lib/systemd/system/*.service.d"
+        ]
+
+        result = _read_glob_paths_with_parser(tree, checked_globs,
+                                              SystemdServiceDropins.read_systemd_service_dropin)
+        if result:
+            return cls(result)
+        return None
+
+    @classmethod
+    def from_json(cls, json_o):
+        config = json_o.get("systemd-service-dropins")
+        if config:
+            return cls(config)
+        return None
