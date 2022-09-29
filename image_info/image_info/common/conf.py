5db4a1b8e6817821c12329c03e1dbdd0a911e1f1
Thomas Lavocat <tlavocat@redhat.com>
2022-09-29 13:33:17 +0200

    commons: add the tmpfiles.d element

--- image_info/image_info/common/conf.py
+++ image_info/image_info/common/conf.py
@@ -937,3 +937,54 @@ class Modprobe(Common):
         if modprobe:
             return cls(modprobe)
         return None
+
+
+@define(slots=False)
+class Tmpfilesd(Common):
+    """
+    Tmpfilesd
+    """
+    flatten = True
+    tmpfiles__d: dict
+
+    @classmethod
+    def explore(cls, tree, _is_ostree=False):
+        """
+        Read all tmpfiles.d *.conf files from a predefined list of paths and parse
+        them.
+
+        The searched paths are:
+        - "/etc/tmpfiles.d/*.conf"
+        - "/usr/lib/tmpfiles.d/*.conf"
+
+        Returns: dictionary as returned by '_read_glob_paths_with_parser()' with
+        configuration representation as returned by 'read_tmpfilesd_config()'.
+
+        An example return value:
+        {
+            "/etc/tmpfiles.d": {
+                "sap.conf": [
+                    "x /tmp/.sap*",
+                    "x /tmp/.hdb*lock",
+                    "x /tmp/.trex*lock"
+                ]
+            }
+        }
+        """
+        checked_globs = [
+            "/etc/tmpfiles.d/*.conf",
+            "/usr/lib/tmpfiles.d/*.conf"
+        ]
+
+        result = _read_glob_paths_with_parser(
+            tree, checked_globs, read_tmpfilesd_config)
+        if result:
+            return cls(result)
+        return None
+
+    @classmethod
+    def from_json(cls, json_o):
+        tmpf = json_o.get("tmpfiles.d")
+        if tmpf:
+            return cls(tmpf)
+        return None
