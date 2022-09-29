ccb296a941d2d5dbad56129c6ba95417ef98cde3
Thomas Lavocat <tlavocat@redhat.com>
2022-09-29 13:15:36 +0200

    commons: add the locale element

--- image_info/image_info/common/env.py
+++ image_info/image_info/common/env.py
@@ -102,3 +102,37 @@ class Timezone(Common):
         if timezone:
             return cls(timezone)
         return None
+
+
+@define(slots=False)
+class Locale(Common):
+    """
+    Locale
+    """
+    flatten = True
+    locale: str
+
+    @classmethod
+    def explore(cls, tree, _is_ostree=False):
+        """
+        Read all uncommented key/values set in /etc/locale.conf.
+
+        Returns: dictionary with key/values read from the configuration file.
+        The returned dictionary may be empty.
+
+        An example return value:
+        {
+            "LANG": "en_US"
+        }
+        """
+        with contextlib.suppress(FileNotFoundError):
+            with open(f"{tree}/etc/locale.conf") as f:
+                return cls(parse_environment_vars(f.read()))
+        return None
+
+    @classmethod
+    def from_json(cls, json_o):
+        locale = json_o.get("locale")
+        if locale:
+            return cls(locale)
+        return None
