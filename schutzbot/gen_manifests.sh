a481841705d262e47325d6b003ccbbb0d4b78af1
Jakub Rusz <jrusz@redhat.com>
2024-01-23 09:37:39 +0100

    Change cache directory for manifest gen
    The fedora runner doesn't have enough space in /tmp as it's using tmpfs.

--- schutzbot/gen_manifests.sh
+++ schutzbot/gen_manifests.sh
@@ -7,4 +7,4 @@ echo "Installing build dependencies"
 sudo dnf install -y redhat-rpm-config btrfs-progs-devel device-mapper-devel
 sudo dnf build-dep -y osbuild-composer
 echo "Generating manifests"
-go run ./cmd/gen-manifests -workers 50 -output ../manifests
+go run ./cmd/gen-manifests -workers 50 -output ../manifests --cache /var/tmp/rpmmd
